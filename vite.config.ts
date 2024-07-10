208cdbbc775c28ac90705c949fbc8dfe55ad328c
Arthur Green <curious.dev@icloud.com>
2024-07-10 18:58:26 +0530

    chore: add trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     remix({
       ssr: false,
-      basename: '/star-network',
+      basename: '/star-network/',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
